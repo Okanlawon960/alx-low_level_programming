84d4275d5faeb69e653a7243b78af4d040f8e6f7
hammed <Okanlawonhammed960@gmail.com>
2023-06-20 18:12:42 -0700

    c level programming

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - Check the dir README
+ *
+ * @c: The character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
