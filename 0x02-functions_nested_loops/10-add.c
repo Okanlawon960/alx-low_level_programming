66a5fc78439f1d34998fbd813d482a88c4239baf
hammed <Okanlawonhammed960@gmail.com>
2023-06-20 18:26:20 -0700

    c level programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Check the dir README
+ *
+ * @num1: The first number parameter
+ *
+ * @num2: The second number parameter
+ *
+ * Return: Return the sum of the two number parameters
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
