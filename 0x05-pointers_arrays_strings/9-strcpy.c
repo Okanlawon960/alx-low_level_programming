1811cef76567490c025c76a3ea0024f9fff019e3
hammed <Okanlawonhammed960@gmail.com>
2023-06-26 18:12:17 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - Kindly refer to the dir README
+ *
+ * @dest: Destination buffer
+ * @src: Source string to be copied
+ *
+ * Return: Pointer to the destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
