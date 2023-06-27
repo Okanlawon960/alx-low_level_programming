9e4093dd7927c9b28b325c5892a35cbfa8d0098f
hammed <Okanlawonhammed960@gmail.com>
2023-06-26 17:59:55 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Kindly refer to the dir README file
+ *
+ * @s: The string paramenter pass to the function
+ *
+ * Return: Return the length of the string
+ */
+int _strlen(char *s)
+{
+	size_t length = strlen(s);
+
+	return (length);
+}
