71b42a54971d5aff578878a8be885625490c29f3
hammed <Okanlawonhammed960@gmail.com>
2023-06-26 18:02:44 -0700

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Kindly refer to the dir README file
+ *
+ * @str: The parameter string that is passed to the function
+ *
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
