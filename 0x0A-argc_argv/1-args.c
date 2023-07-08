9b643b46374334e67121299d9e632d482d1a56e4
hammed <Okanlawonhammed960@gmail.com>
2023-07-07 17:01:32 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+	return (0);
+}
